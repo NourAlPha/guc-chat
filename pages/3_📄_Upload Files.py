661f5046bfef51af5934e1d5f8980bf78dab97a3
nouralpha <nourayman189@gmail.com>
2024-05-14 11:48:03 +0300

    add doc, docx, xls, xlsx, txt upload to upload files page

---
+++ pages/3_📄_Upload Files.py
@@ -0,0 +1,92 @@
+import streamlit as st
+from utils import initialize_session_state, process_vector_space
+from auth import authenticate_admin
+import textract
+import os
+
+def main():
+    # Set page configuration including title and icon
+    st.set_page_config(page_title="Upload PDF",
+                        page_icon="📄")
+    
+    # Authenticate admin
+    authenticate_admin()
+    if "authentication_status" not in st.session_state or not st.session_state.authentication_status:
+        return
+
+    # Initialize session state with needed variables
+    initialize_session_state()
+
+    if "uploader_key" not in st.session_state:
+        st.session_state.uploader_key = 0
+
+    # Add a title to the page
+    st.title("📄 Upload Files")
+
+    # Add a description to the page
+    st.markdown("Upload file(s) to add content to my knowledge base.")
+
+    # Add a file uploader to the page
+    uploaded_file = st.file_uploader(
+        "Choose a PDF file",
+        type=["pdf", "doc", "docx", "txt", "xls", "xlsx"],
+        accept_multiple_files=True,
+        key=f"uploader_{st.session_state.uploader_key}",
+    )
+
+    def process_files():
+        if not uploaded_file:
+            return
+        for file in uploaded_file:
+            if file.type == "application/pdf":
+                process_pdf_files(file)
+            elif file.type == "application/msword":
+                process_doc_files(file)
+            elif file.type == "text/plain":
+                process_text_files(file)
+            elif file.type == "application/vnd.ms-excel":
+                process_excel_files(file)
+            else:
+                st.warning(f"Unsupported file type: {file.type}")
+        process_vector_space()
+        st.session_state.uploader_key += 1
+    
+    def process_pdf_files(file):
+        if not uploaded_file:
+            return
+        with open(f"pdf_files/{file.name}", "wb") as f:
+            f.write(file.getbuffer())
+    
+    def process_doc_files(file):
+        if not uploaded_file:
+            return
+        with open(f"temp_files/{file.name}", "wb") as f:
+            f.write(file.getbuffer())
+        text = textract.process(f"doc_files/{file.name}")
+        file_name = file.name.split(".")[0]
+        with open(f"text_files/{file_name}.txt", "w") as f:
+            f.write(text.decode("utf-8"))
+        os.remove(f"temp_files/{file.name}")
+    
+    def process_excel_files(file):
+        if not uploaded_file:
+            return
+        with open(f"temp_files/{file.name}", "wb") as f:
+            f.write(file.getbuffer())
+        text = textract.process(f"excel_files/{file.name}")
+        file_name = file.name.split(".")[0]
+        with open(f"text_files/{file_name}.txt", "w") as f:
+            f.write(text.decode("utf-8"))
+        os.remove(f"temp_files/{file.name}")        
+    
+    def process_text_files(file):
+        if not uploaded_file:
+            return
+        with open(f"text_files/{file.name}", "w") as f:
+            f.write(file.getvalue().decode("utf-8"))
+
+    if st.button("Save to Database", on_click=process_files):
+        st.success("PDF file(s) uploaded successfully! 📄🚀")
+
+if __name__ == "__main__":
+    main()
