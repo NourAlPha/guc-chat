d0f3e5c76921f43e03bc5442842975498fac4f84
nouralpha <nourayman189@gmail.com>
2024-06-16 15:07:22 +0300

    Add feedback component to the app

--- pages/1_💭_Chat With Me.py
+++ pages/1_💭_Chat With Me.py
@@ -1,8 +1,24 @@
 import streamlit as st
 from utils import make_output, modify_output, initialize_session_state
+from streamlit_feedback import streamlit_feedback
+import json
 
 # Initialize session state with needed variables
 initialize_session_state()
+
+score_emoji_map = {
+    "😀": "Great",
+    "🙂": "Good",
+    "😐": "Average",
+    "🙁": "Poor",
+    "😞": "Very Poor",
+}
+
+def _submit_feedback(user_feedback):
+    feedbacks = json.load(open("feedback.json"))
+    feedbacks[score_emoji_map[user_feedback["score"]]] += 1
+    with open("feedback.json", "w") as f:
+        json.dump(feedbacks, f)
 
 def main():
     # Set page configuration including title and icon
@@ -14,9 +30,19 @@ def main():
     if st.sidebar.button("🔄 Refresh"):
         st.session_state.messages = []
     # Display previous chat messages
-    for message in st.session_state.messages:
+    for n, message in enumerate(st.session_state.messages):
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
+            if message["role"] == "assistant":
+                feedback_key = f"feedback_{int(n/2)}"
+                if feedback_key not in st.session_state:
+                    st.session_state[feedback_key] = None
+                streamlit_feedback(
+                    feedback_type="faces",
+                    key=feedback_key,
+                    on_submit=_submit_feedback,
+                    align="center",
+                )
     # Accept user input in the chat interface
     if prompt := st.chat_input("💭 What's in your mind?"):
         # Display user input as a chat message
@@ -34,6 +60,7 @@ def main():
             st.write_stream(modify_output(response))
         # Append chatbot response to session state
         st.session_state.messages.append({"role": "assistant", "content": response})
+        st.rerun()
 
 
 if __name__ == "__main__":
---
+++ pages/5_📊_Feedback Statistics.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import plotly.graph_objects as go
+import json
+from auth import authenticate_admin
+from utils import initialize_session_state
+
+def main():
+    
+    # Set page configuration including title and icon
+    st.set_page_config(page_title="Feedback Statistics",
+                        page_icon="📊")
+    
+    # Authenticate admin
+    if "authentication_status" not in st.session_state or not st.session_state.authentication_status:
+        authenticate_admin()
+    else:
+        st.session_state.authenticator.logout('Logout', 'sidebar')
+    if not st.session_state.authentication_status:
+        return
+    
+    # Initialize session state with needed variables
+    initialize_session_state()
+    
+    # Add a title to the page
+    st.title("📊 Feedback Statistics")
+    
+    # Add a description to the page
+    st.markdown("Below is the user feedback statistics of the chatbot responses, with Great being the highest and Very Poor being the lowest.")
+    
+    feedbacks = json.load(open("feedback.json"))
+    labels = list(feedbacks.keys())
+    values = list(feedbacks.values())
+
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    fig.update_layout(height=600)
+    st.plotly_chart(fig, theme=None, height=800)
+
+if __name__ == "__main__":
+    main()
