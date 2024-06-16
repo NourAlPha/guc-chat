f35b64111f1107c6157afe8aa8a36801740a825b
nouralpha <nourayman189@gmail.com>
2024-06-16 22:10:27 +0300

    Add regenerate button in chat

--- pages/1_💭_Chat With Me.py
+++ pages/1_💭_Chat With Me.py
@@ -21,6 +21,17 @@ def _submit_feedback(user_feedback, feedback_key):
     with open("feedback.json", "w") as f:
         json.dump(feedbacks, f)
     st.session_state["_" + feedback_key] = user_feedback
+    
+def insert_new_id():
+    st.session_state.id_counter += 1
+    st.session_state.id_list.append(st.session_state.id_counter)
+
+def delete_from_session_state(key):
+    if key in st.session_state:
+        del st.session_state[key]
+
+def disable_buttons():
+    st.session_state.making_output = True
 
 def main():
     # Set page configuration including title and icon
@@ -28,18 +39,44 @@ def main():
                     page_icon="🤔")
     # Display the title of the chat interface
     st.title("💭 What's in your mind?")
-    # Add a refresh button to clear the chat interface
-    if st.sidebar.button("🆕 New Chat"):
-        for n in range(int(len(st.session_state.messages)/2)):
-            feedback_key = f"feedback_{n}"
-            st.session_state["_" + feedback_key] = None
+    
+    col1, col2 = st.sidebar.columns([1, 1])
+    
+    if "regenerating" not in st.session_state:
+        st.session_state.regenerating = False
+        st.session_state.making_output = False
         st.session_state.messages = []
+        st.session_state.id_list = []
+        st.session_state.id_counter = 0
+    
+    # Add a refresh button to clear the chat interface
+    if col1.button("🆕 New Chat", disabled=st.session_state.making_output):
+        for id in st.session_state.id_list:
+            feedback_key = f"feedback_{id}"
+            delete_from_session_state(feedback_key)
+            delete_from_session_state("_" + feedback_key)
+            delete_from_session_state("feedback_submitted_" + feedback_key)
+        st.session_state.id_list = []
+        st.session_state.messages = []
+        
+    if col2.button("🔄 Regenerate", on_click=disable_buttons, disabled=st.session_state.making_output):
+        if len(st.session_state.id_list) > 0:
+            feedback_key = f"feedback_{st.session_state.id_list[-1]}"
+            delete_from_session_state(feedback_key)
+            delete_from_session_state("_" + feedback_key)
+            delete_from_session_state("feedback_submitted_" + feedback_key)
+            st.session_state.messages = st.session_state.messages[:-1]
+            st.session_state.id_list = st.session_state.id_list[:-1]
+            st.session_state.regenerating = True
+    
     # Display previous chat messages
     for n, message in enumerate(st.session_state.messages):
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
             if message["role"] == "assistant":
-                feedback_key = f"feedback_{int(n/2)}"
+                if int(n/2) >= len(st.session_state.id_list):
+                    insert_new_id()
+                feedback_key = f"feedback_{st.session_state.id_list[int(n/2)]}"
                 if ("_" + feedback_key) in st.session_state:
                     st.session_state[feedback_key] = st.session_state["_" + feedback_key]
                 if feedback_key not in st.session_state:
@@ -53,12 +90,16 @@ def main():
                     disable_with_score=st.session_state[feedback_key]["score"] if st.session_state[feedback_key] else None,
                 )
     # Accept user input in the chat interface
-    if prompt := st.chat_input("💭 What's in your mind?"):
+    if prompt := st.chat_input("💭 What's in your mind?", on_submit=disable_buttons) or st.session_state.regenerating:
         # Display user input as a chat message
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        # Append user input to session state
-        st.session_state.messages.append({"role": "user", "content": prompt})
+        if not st.session_state.regenerating:
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            # Append user input to session state
+            st.session_state.messages.append({"role": "user", "content": prompt})
+        
+        if st.session_state.regenerating:
+            prompt = st.session_state.messages[-1]["content"]
         
         # Get response from the chatbot based on user input
         response = make_output(prompt)
@@ -69,7 +110,9 @@ def main():
             st.session_state.messages.append({"role": "assistant", "content": response})
             # Write response with modified output (if any)
             st.write_stream(modify_output(response))
-
+        
+        st.session_state.regenerating = False
+        st.session_state.making_output = False
         st.rerun()
 
 
